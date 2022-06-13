4a64a3e65d19ed7538347c1d99bc8ab895de93f5
No Name <dev.haxuan@gmail.com>
2022-06-13 21:12:37 +0700

    add project

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,6 @@
 export const aboutData = {
     title: "Tôi là ai",
     description1: "Xin chào! Tôi tên là Xuân. Tôi sinh ra và lớn lên ở Bắc Giang, hiện tại tôi đang sống và làm việc ở Hà Nội.",
-    description2: "Tôi học lập trình máy tính và thiết bị di động tại trường Cao đẳng FPT Polytechnic. Hiện tại tôi là 1 nhà phát triển Website and Mobile Aplication với công nghệ là ReactJs và React Native." +
-        " Tôi là 1 người yêu công nghệ, thích tìm tòi các công nghệ mới để học hỏi." +
-        " Ngoài điều này, tôi còn là người thích đọc sách.",
+    description2: "Tôi học lập trình máy tính và thiết bị di động tại trường Cao đẳng FPT Polytechnic. Hiện tại tôi là 1 nhà phát triển Website and Mobile Aplication với công nghệ là ReactJs và React Native. Tôi là 1 người yêu công nghệ, thích tìm tòi các công nghệ mới để học hỏi. Ngoài điều này, tôi còn là người thích đọc sách.",
     image: 2
 }
